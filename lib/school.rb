e033cdb390465184b3925f54728b1ac672499c6e
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 23:22:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,69 @@
-# code here!
+# # code here!
+# class School
+  
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+  
+#   def name
+#     @name
+#   end
+  
+#   def roster
+#     @roster
+#   end
+  
+#   def add_student(name, grade)
+#     @student_name = name
+#     @grade = grade
+#     @roster[grade] ||= @roster[grade] = [] 
+#     @roster[grade] << name
+#   end
+  
+#   def grade(number)
+#     @roster[number]
+#   end
+  
+#   def sort 
+#     @roster.each do |grade, name|
+#       @roster[grade] = name.sort
+#     end
+#   end
+  
+# # end
+
+# require 'pry'
+
+
+
+
+
+
+
 class School
   
-  def initialize(name)
-    @name = name
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
-  end
-  
-  def name
-    @name
   end
   
   def roster
     @roster
   end
   
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if roster.include?(grade) == false 
-      @roster[grade] = []
-    end
-    @roster[grade] << name
+  def add_student(student_name, grade)
+    @roster[grade] ||= @roster[grade] = []
+    @roster[grade] << student_name
   end
   
-  def grade(number)
-    @roster[number]
+  def grade(grade)
+    @roster[grade]
   end
   
-  def sort 
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+  def sort
+    @roster.each do |grade, student_name|
+      @roster[grade] = student_name.sort
     end
   end
   
